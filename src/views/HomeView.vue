29d1fe325874e2520410ed6775119b0fd4db481a
“kevin.chen” <kevin.chen@medicrowd.com.tw>
2024-03-13 17:25:02 +0800

    setup homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,92 @@
 <template>
   <v-app id="home">
     <NavBar />
-    <v-container fluid> </v-container>
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative" class="mt-16">
+              <h1 class="text-grey">Hello,</h1>
+              <h1 class="text-white">I'm Kevin Chen</h1>
+              <span class="text-grey">Web Designer & Developer</span>
+              <br />
+              <v-btn dark class="text-yellow mt-8" variant="outlined">
+                Contact me
+              </v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div
+              style="
+                position: absolute;
+                z-index: 9999;
+                bottom: 0;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              "
+            >
+              <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <div style="position: relative; z-index: 9999" class="mt-16">
+              <v-img src="favicon.ico" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="favicon.ico" max-height="300"></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About Me</h5>
+              <div style="width: 120px">
+                <v-slider v-model="slider2" color="yellow"></v-slider>
+              </div>
+              <h4 class="mt-n4">I am a Creative Web Designer</h4>
+              <h4>and Developer</h4>
+              <p class="text-grey">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
+                vero, cumque, at illo, officia a dicta dolore temporibus ipsa
+                harum dolorum ducimus quam laboriosam? Quis dolor placeat
+                obcaecati aperiam natus.
+              </p>
+              <br />
+              <p class="text-grey">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
+                vero, cumque, at illo, officia a dicta dolore temporibus ipsa
+                harum dolorum ducimus quam laboriosam? Quis dolor placeat
+                obcaecati aperiam natus.
+              </p>
+              <v-btn dark color="yellow" class="mt-4"> Download Resume </v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <div class="text-center mt-4">
+        <h2>What we Do</h2>
+        <div style="width: 120px; margin: 0 auto">
+          <v-slider v-model="slider2" color="yellow"></v-slider>
+        </div>
+      </div>
+      <v-col cols="12" class="padd" id="portfolio">
+        <div class="first" id="project">
+          <v-row>
+            <v-col cols="12">
+              <div class="child"></div>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+    </v-container>
   </v-app>
 </template>
 
@@ -13,9 +98,84 @@ import NavBar from "../components/NavBar.vue";
 
 export default defineComponent({
   name: "HomeView",
+  setup() {
+    return {
+      slider2: 50,
+      items: [
+        {
+          img: "favicon.ico",
+        },
+        {
+          img: "favicon.ico",
+        },
+        {
+          img: "favicon.ico",
+        },
+        {
+          img: "favicon.ico",
+        },
+        {
+          img: "favicon.ico",
+        },
+        {
+          img: "favicon.ico",
+        },
+      ],
+    };
+  },
 
   components: {
     NavBar,
   },
 });
 </script>
+
+<style scoped>
+.v-container {
+  padding: 16px 0;
+}
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  width: 100%;
+  height: 400px;
+  color: white;
+}
+.head::before {
+  content: " ";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+.head::after {
+  content: " ";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background: #fbdf7e;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+.first {
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+</style>
